4ff1273ba6c2a2af6207f9b99cd656d5b2e454b1
Terekhin <blackrat72@qip.ru>
2022-11-23 15:57:25 +0300

    format code

--- task2.py
+++ task2.py
@@ -2,9 +2,8 @@
 #     ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 # Нужно написать таблицу истинности.
 
-for x in [True,False]:
-    for y in [True,False]:
-        for z in [True,False]:
-            result = not(x or y or z) == ((not x) and (not y) and (not z))
-            print (f'x = {x}, y = {y}, z = {z}  Result = {result}')
-        
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            result = not (x or y or z) == ((not x) and (not y) and (not z))
+            print(f'x = {x}, y = {y}, z = {z}  Result = {result}')

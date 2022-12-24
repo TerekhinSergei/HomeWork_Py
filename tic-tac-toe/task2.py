dfb6b205a0b497405d9653dbe2760cd1d1e8a11e
Terekhin <blackrat72@qip.ru>
2022-12-24 18:10:00 +0300

    after autopep8

--- tic-tac-toe/task2.py
+++ tic-tac-toe/task2.py
@@ -8,12 +8,12 @@ if check.lower() == 'д' or check.lower() == 'y':
     while game:
         try:
             TTT3.play()
-        except: # в скачанной библиотеке есть ошибки, поэтому обернул
-            print('что-то пошло не так, заходи ещё') 
-        check = input("Еще разок?! Усли согласен просто нажми <интер>\nиначе 'Н' или 'N' : ")     
+        except:  # в скачанной библиотеке есть ошибки, поэтому обернул
+            print('что-то пошло не так, заходи ещё')
+        check = input("Еще разок?! Усли согласен просто нажми <интер>\nиначе 'Н' или 'N' : ")
         if check.lower() == 'н' or check.lower() == 'n':
             game = False
-else:  
+else:
     check = input("Желаете ознакомиться со своей статистикой - 'Д' or 'Y', нет - просто <интер>: ")
     if check.lower() == 'д' or check.lower() == 'y':
         TTT3.showProbability()
